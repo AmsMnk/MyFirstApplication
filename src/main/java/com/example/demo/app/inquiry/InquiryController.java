c55adf2684d984e030bb5d2737ee6af2ebc61e56
maho karino <m.karino0088@gmail.com>
2023-12-28 18:17:05 +0900

    dao

--- src/main/java/com/example/demo/app/inquiry/InquiryController.java
+++ src/main/java/com/example/demo/app/inquiry/InquiryController.java
@@ -1,5 +1,9 @@
 package com.example.demo.app.inquiry;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -10,52 +14,70 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.example.demo.entity.Inquiry;
+import com.example.demo.service.InquiryService;
+
 @Controller
 @RequestMapping("/inquiry")
 public class InquiryController {
-	
+
+	private final InquiryService inquiryService;
+
+	@Autowired
+	public InquiryController(InquiryService inquiryService) {
+		this.inquiryService = inquiryService;
+	}
+
+	@GetMapping
+	public String index(Model model) {
+		List<Inquiry> list = inquiryService.getAll();
+
+		model.addAttribute("inquiryList", list);
+		model.addAttribute("title", "Inquiry Index");
+
+		return "inquiry/index";
+	}
+
 	@GetMapping("/form")
-	public String form(InquiryForm inquiryForm,
-			Model model,
-			@ModelAttribute("complete") String complete) {
+	public String form(InquiryForm inquiryForm, Model model, @ModelAttribute("complete") String complete) {
 		model.addAttribute("title", "Inquiry Form");
 		return "inquiry/Form";
 	}
-	
+
 	@PostMapping("/form")
 	public String formGoBack(InquiryForm inquiryForm, Model model) {
 		model.addAttribute("title", "Inquiry Form");
 		return "inquiry/Form";
 	}
-	
+
 	@PostMapping("/confirm")
-	public String confirm(@Validated InquiryForm inquiryForm, 
-			BindingResult result,
-			Model model) {
-		
-		if(result.hasErrors()) {
+	public String confirm(@Validated InquiryForm inquiryForm, BindingResult result, Model model) {
+
+		if (result.hasErrors()) {
 			model.addAttribute("title", "Inquiry Form");
 			return "inquiry/form";
 		}
-		
+
 		model.addAttribute("title", "Confirm Page");
 		return "inquiry/confirm";
 	}
-	
+
 	@PostMapping("/complete")
-	public String complete(
-			@Validated InquiryForm inquiryForm, 
-			BindingResult result,
-			Model model,
+	public String complete(@Validated InquiryForm inquiryForm, BindingResult result, Model model,
 			RedirectAttributes redirectAttributes) {
-		
-		if(result.hasErrors()) {
+
+		if (result.hasErrors()) {
 			model.addAttribute("title", "Inquiry Form");
 			return "inquiry/form";
 		}
-		
-		//insert into the database
-		
+
+		Inquiry inquiry = new Inquiry();
+		inquiry.setName(inquiryForm.getName());
+		inquiry.setEmail(inquiryForm.getEmail());
+		inquiry.setContents(inquiryForm.getContents());
+		inquiry.setCreated(LocalDateTime.now());
+
+		inquiryService.save(inquiry);
 		redirectAttributes.addFlashAttribute("complete", "Registerd!");
 		return "redirect:/inquiry/form";
 	}
